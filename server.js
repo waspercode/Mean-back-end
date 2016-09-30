7335aacd715dcc578f99400bda777543f8b11edb
Ravi Yerramilli <raviyerramilli@hotmail.com>
2016-09-30 20:44:14 +0530

    create a message list end point with express

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(function(req,res,next){
     next();
 })
 
+app.get('/api/message', GetMessages)
+
 app.post('/api/message', function(req,res){
     console.log(req.body);
 
@@ -25,17 +27,16 @@ app.post('/api/message', function(req,res){
     res.status(200);
 })
 
-function GetMessages()
+function GetMessages(req,res)
 {
     Message.find({}).exec(function(err, result){
-        console.log(result);
+        res.send(result);
     })
 }
 
 mongoose.connect("mongodb://localhost:27017/test", function(err,db){
     if(!err){
         console.log("we are connected to mongo");
-        GetMessages();
     }
 })
 
